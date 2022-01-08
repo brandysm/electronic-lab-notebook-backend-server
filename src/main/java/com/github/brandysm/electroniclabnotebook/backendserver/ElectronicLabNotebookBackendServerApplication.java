c83502eb4b8993979e3c582890fabe3f96585e89
Michał Brandys <michalb@jawornet.pl>
2022-01-08 20:31:16 +0100

    Initiate project

---
+++ src/main/java/com/github/brandysm/electroniclabnotebook/backendserver/ElectronicLabNotebookBackendServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.brandysm.electroniclabnotebook.backendserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicLabNotebookBackendServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicLabNotebookBackendServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/brandysm/electroniclabnotebook/backendserver/ElectronicLabNotebookBackendServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.brandysm.electroniclabnotebook.backendserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicLabNotebookBackendServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
